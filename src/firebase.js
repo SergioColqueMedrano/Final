0b00aeb6e8637772537fa19677cdc3d1c4f980de
Ezequiel4larcon <alarconezequiel@yahoo.com.ar>
2022-06-26 03:55:50 -0300

    Segundo

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8OKNBWRfv1kQhobnOdtES4VmLzgj--l0",
+  authDomain: "tp-final-prog3.firebaseapp.com",
+  projectId: "tp-final-prog3",
+  storageBucket: "tp-final-prog3.appspot.com",
+  messagingSenderId: "253341748100",
+  appId: "1:253341748100:web:1b38b1c832a8a0e0a66bd1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
