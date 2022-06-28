20c90aa3cf4ead01c4b5a8c49cfa81459e152a07
TOMY <tomyvogel@gmail.com>
2022-06-28 02:27:41 -0300

    Header del home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 export function Home() {
   return (
-    <div>Home</div>
+    <div className="w-full h-24 bg-green-800 mx-auto">
+
+      <div className="absolute mt-2 h-20 w-6/12 bg-[url(https://sanatorioadventista.org.ar/assets/img/footer_logo_sap.png)] bg-no-repeat"></div>
+
+      <div className="mt-3 w-fit h-16 bg-pink-800 ">
+        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-3 h-full">Gestionar Socios</button>
+        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-3.5 h-full">Gestionar Adherentes</button>
+        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-3.5 h-full">Gestionar Cobros</button>
+      </div>
+      
+    </div>
   )
 }
 
