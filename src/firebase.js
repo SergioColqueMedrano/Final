a840af10fcc6ef1f73b7e7eb63611b76dd9bd1ba
TOMY <tomyvogel@gmail.com>
2022-06-28 16:45:51 -0300

    Base de datos conectada

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC8OKNBWRfv1kQhobnOdtES4VmLzgj--l0",
   authDomain: "tp-final-prog3.firebaseapp.com",
+  databaseURL: "https://tp-final-prog3-default-rtdb.firebaseio.com",
   projectId: "tp-final-prog3",
   storageBucket: "tp-final-prog3.appspot.com",
   messagingSenderId: "253341748100",
@@ -17,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = app.firestore();
 
 export async function login(email, password) {
   const userCredentials = await signInWithEmailAndPassword(
