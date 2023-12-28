dee83de8dcdaecc4db683d3ea1dad1c73d07b6a3
Sergio Colque <raidanearth@gmail.com>
2023-12-28 12:21:28 -0300

    logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const { theme } = require('tailwindcss/stubs/defaultConfig.stub');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'body-pattern': "url('../img/adventista.png')"
+      })
+    },
+  },
+  variants: {
+
   },
   plugins: [],
 }
