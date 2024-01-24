e5b2446f12ff7ff5f4143ad0bccc9c5ac6d70625
Sergio Colque <raidanearth@gmail.com>
2024-01-24 16:02:56 -0300

    Centralizar la validacion del rol

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch, } = require('../controllers/usuarios');
 const { check } = require('express-validator');
 
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
+
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -13,6 +15,7 @@ router.post('/',[
     check('correo', 'El correo no es válido').isEmail(),
     check('password', 'El password debe ser de más de 6 letras').isLength({ min: 6 }),
     //check('rol', 'No es rol valido'),isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom( esRoleValido ),
     validarCampos
 ], usuariosPost)
 
